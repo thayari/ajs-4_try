c766d892cad3f7c76b63dcadc27992da1256fc6e
thayari <alex.silvanovich@gmail.com>
2019-12-16 23:09:56 +0300

    fixed tests

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ export default function checkNumber(input) {
   let output = 0;
   const inputValue = Number(input);
   try {
-    if (!Number.isNaN(inputValue) && Number.isInteger(inputValue)) {
+    if (!Number.isNaN(inputValue) && Number.isInteger(inputValue) && input[0] !== '0') {
       output = inputValue;
     } else {
       throw new Error(`${input} is not a proper number`);
--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,26 @@ test('string to number', () => {
   expect(checkNumber('42')).toBe(42);
 });
 
-test('string to number error', () => {
+test('string to number error - letters', () => {
   expect(checkNumber('qwerty')).toBe(0);
+});
+
+test('string to number error - fractional', () => {
+  expect(checkNumber('123.456')).toBe(0);
+});
+
+test('string to number error - binary', () => {
+  expect(checkNumber('0b0111')).toBe(0);
+});
+
+test('string to number error - hex', () => {
+  expect(checkNumber('0x12')).toBe(0);
+});
+
+test('string to number error - oct', () => {
+  expect(checkNumber('0755')).toBe(0);
+});
+
+test('string to number error - letters and numbers', () => {
+  expect(checkNumber('qwerty123')).toBe(0);
 });
